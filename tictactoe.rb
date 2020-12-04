9ecad6f50f37925b639064ad38915817e0e3c58d
Elliot Connell <elliot.connell@gmail.com>
2020-12-04 16:11:50 +1100

    created the game board

--- tictactoe.rb
+++ tictactoe.rb
@@ -4,6 +4,26 @@
 # the board has nine empty squares 
 # => the board is an array of 9 empty squares
 
+puts board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+def display_board
+  puts row = ["   " "|" "   " "|" "   "]
+  puts seperator = "-----------"
+  puts row
+  puts seperator
+  puts row
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+display_board(board)
+
 
 # there are two different tokens - X and O
 # => X & 0
