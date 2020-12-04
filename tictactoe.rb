a10fba6f07602e00c4ed1cf1813c06a8fa5b57d2
Elliot Connell <elliot.connell@gmail.com>
2020-12-04 14:32:12 +1100

    first commit of code. contains psuedo code

---
+++ tictactoe.rb
@@ -0,0 +1,49 @@
+# tic tac toe
+
+# the board
+# the board has nine empty squares 
+
+
+# there are two different tokens - X and O
+
+
+
+# there are two players
+
+
+
+# players take turns playing
+
+
+
+# 1st trun - player 1
+
+
+
+# player 1 places token (x) in only 1 square of their choice
+
+
+
+# 2nd turn - player 2
+
+
+
+# player 2 places their token (0) in only one square of their choice
+# can't be placed in square with token
+
+
+
+# 3rd turn - player 1...  repeat cycle until result
+
+
+
+
+# 8 winning combinations: 
+#top row, middle row, bottom row, Left column, middle column, right column, left diagonal, and right diagonal
+
+
+
+
+# there's a draw/ tie when the entire board is filled but there are no winning combinations
+
+
